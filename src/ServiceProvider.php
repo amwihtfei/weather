5a77c59983366cc4603de456362a4c57b9ebf9e0
amwihtfei <amwihtfei@163.com>
2019-03-28 02:54:17 +0000

    Apply fixes from StyleCI

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,20 +1,30 @@
 <?php
 
-    namespace man30\weather;
-    class ServiceProvider extends \Illuminate\Support\ServiceProvider
+/*
+ * This file is part of the man30/weather.
+ *
+ * (c) overtrue <i@man30.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace man30\weather;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    protected $defer = true;
+
+    public function register()
     {
-        protected $defer = TRUE;
-
-        public function register()
-        {
-            $this->app->singleton(Weather::class, function () {
-                return new Weather(config('services.weather.key'));
-            });
-            $this->app->alias(Weather::class, 'weather');
-        }
-
-        public function provides()
-        {
-            return [Weather::class, 'weather'];
-        }
+        $this->app->singleton(Weather::class, function () {
+            return new Weather(config('services.weather.key'));
+        });
+        $this->app->alias(Weather::class, 'weather');
     }
+
+    public function provides()
+    {
+        return [Weather::class, 'weather'];
+    }
+}
